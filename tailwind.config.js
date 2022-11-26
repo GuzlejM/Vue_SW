1587fb2b758c22b4f8badbd615ba1da8bd7b0ef6
GuzlejM <martin.guzlej@gmail.com>
2022-11-26 02:00:29 +0100

    Added char card hover effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,13 +17,19 @@ module.exports = {
       margin: {
         responsive: "m-3 sm:m-6 md:mx-8 lg:m-5 lg:mx-24",
       },
+      scale: {
+        // eslint-disable-next-line prettier/prettier
+        3: "3",
+        "07": "0.7",
+      },
     },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
     },
     boxShadow: {
-      card_default: "0 6px 14px 0px rgba(46, 45, 57, 0.17)",
-      card_hovered: "0 20px 34px 0px rgba(46, 45, 57, 0.17)",
+      card_list_default: "0 6px 14px 0 rgba(46, 45, 57, 0.17)",
+      card_list_hovered: "0 20px 34px 0 rgba(46, 45, 57, 0.17)",
+      card_img_hovered: "0 0 0 14px rgba(250, 250, 250)",
     },
   },
   plugins: [],
